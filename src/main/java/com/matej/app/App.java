9cb9c055adf074a3a1a7474f5af028f65283f957
Matej Vrecar <matej.vrecar.2020@uni.strath.ac.uk>
2022-03-03 18:55:54 +0000

    First edition of the software. Bushwack dictionary

--- src/main/java/com/matej/app/App.java
+++ src/main/java/com/matej/app/App.java
@@ -1,13 +1,42 @@
 package com.matej.app;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static String link = "https://jisho.org/api/v1/search/words?keyword=";
+
+    public static void main( String[] args ) throws IOException, InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a word");
+        String word = scanner.nextLine();
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .header("Content-Type", "application/json")
+                .uri(URI.create(link + word))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(response.body().toString());
+
+        System.out.println(jsonNode.get("data").get(0).get("slug") + " - " + jsonNode.get("data").get(0).get("japanese").get(0).get("reading") + " - " +
+                jsonNode.get("data").get(0).get("senses").get(0).get("parts_of_speech").get(0));
     }
 }
